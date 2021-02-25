b6afcaacd7c09e4e13bb298534d36e4ea86302d1
DESKTOP-2BJM56G\Hyeseon <artsmenii@gmail.com>
2021-02-25 11:51:08 +0900

    #develop_delete_2120225 #1 bdelete commit

--- src/main/java/edu/bit/ex/BoardController.java
+++ src/main/java/edu/bit/ex/BoardController.java
@@ -39,4 +39,17 @@ public class BoardController {
 		return "home";
 	}
 	
+	public String boarddelete(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
 }
